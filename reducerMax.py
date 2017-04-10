78a1c56bee91caeea85e3ef8656679d2ac5fc31d
markruddy <drmarkruddy@gmail.com>
2017-04-11 00:19:19 +0100

    added counter

--- reducerMax.py
+++ reducerMax.py
@@ -6,6 +6,7 @@ import sys
 
 salesMax = 0
 oldKey = None
+counter = 0
 
 # print type(sys.stdin)
 
@@ -17,16 +18,27 @@ for line in sys.stdin:
         continue
 
     # Name elements of the array 'data_mapped'
-    thisKey, thisSale = data_mapped 
+    thisKey, thisSale = data_mapped
+
+    counter += 1
 
     if oldKey and oldKey != thisKey:
-        print oldKey, "\t", salesMax
-        oldKey = thisKey;
-        salesMax = 0
+        if counter == 1:
+            print thisKey, "\t", thisSale, "-", counter
+            oldKey = thisKey;
+            salesMax = 0;
+            counter = 0
+        else:
+            print oldKey, "\t", salesMax, "-", counter
+            oldKey = thisKey;
+            salesMax = thisSale;
+            counter = 0
     else:
         if salesMax <= float(thisSale):
             salesMax = float(thisSale);
             oldKey = thisKey
+
+
 
 # Capture last line to finish the loop 
 # if oldKey != None:
