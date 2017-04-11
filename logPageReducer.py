c26a67fcd9849a38fed38695cf13028dcb69bbd6
markruddy <drmarkruddy@gmail.com>
2017-04-11 23:27:27 +0100

    Finished mapper

--- logPageReducer.py
+++ logPageReducer.py
@@ -2,6 +2,30 @@
 
 # Mark Ruddy 
 # 2017-04-11 
-# Reducer to give total for a specific url
+# Reducer to give total count for a specific url
+
+import sys
+
+counter = 0
+url = '/assets/js/the-associates.js'
+
+for line in sys.stdin:
+	data = line.strip().split("\t")
+
+	if len(data) != 2:
+		# Defensive programming. Something has gone wrong. Skip this line.
+		continue
+
+	thisKey, requestUrl = data
+
+	if requestUrl == url:
+		counter += 1
+		# print thisKey, "\t", requestUrl
+print counter
+
+
+
+
+
 
 
