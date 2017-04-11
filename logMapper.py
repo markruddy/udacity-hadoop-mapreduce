280a072ee69afbfe369c669f9286a5d577766c3d
markruddy <drmarkruddy@gmail.com>
2017-04-11 16:54:39 +0100

    Update
    line split, recombine, naming

--- logMapper.py
+++ logMapper.py
@@ -7,7 +7,9 @@
 import sys
 
 for line in sys.stdin:
-    data = line.strip().split(" ")
-	if len(data) == 10:
-		data_mapped = [data[0],data[1],data[2]]
-print len(data_mapped)
+    data = line.strip().split(" ") # Split on non-breaking whitespace
+    if len(data) == 10: # Defensive programming against non-standard log lines
+    	# Combine 
+    	data_mapped = [data[0], data[1], data[2], data[3]+" "+data[4], data[5]+" "+data[6]+" "+data[7], data[8], data[9]]
+    	ipaddress, identity, username, time, request, status, size = data_mapped
+    print "{0}\t{6}".format(ipaddress, status)
