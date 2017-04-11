1c7f7d9157717b8d512c272590a6caf23d1b2a6d
markruddy <drmarkruddy@gmail.com>
2017-04-11 12:14:29 +0100

    Update comments

--- mapperStore.py
+++ mapperStore.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
-
+# Mark Ruddy 
+# 2017-04-11 
 # Format of each line is:
 # date\ttime\tstore name\titem description\tcost\tmethod of payment
-#
-# We need to write them out to standard output, separated by a tab
 
 import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
     if len(data) == 6:
+    	# Write out to standard output, separated by a tab
         date, time, store, item, cost, payment = data
         print "{0}\t{1}".format(store, cost)
 
