110d346c6f1ff931997ff5980a43df8c9670f891
markruddy <drmarkruddy@gmail.com>
2017-04-11 12:09:50 +0100

    Reducer
    Iterate over lines to get max sale amount.

--- reducerMax.py
+++ reducerMax.py
@@ -1,47 +1,36 @@
 #!/usr/bin/python
 
+# Mark Ruddy 
+# 2017-04-11 
 # Reducer to find the maximum value within a key
 
 import sys
 
 salesMax = 0
 oldKey = None
-counter = 0
-
-# print type(sys.stdin)
 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
 
     if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
+        # Defensive programming. Something has gone wrong. Skip this line.
         continue
 
     # Name elements of the array 'data_mapped'
     thisKey, thisSale = data_mapped
 
-    counter += 1
-
-    if oldKey and oldKey != thisKey:
-        if counter == 1:
-            print thisKey, "\t", thisSale, "-", counter
+    if oldKey and oldKey != thisKey: # If keys different we need to print out results of the previous key/value group
+            print oldKey, "\t", salesMax
             oldKey = thisKey;
-            salesMax = 0;
-            counter = 0
-        else:
-            print oldKey, "\t", salesMax, "-", counter
-            oldKey = thisKey;
-            salesMax = thisSale;
-            counter = 0
-    else:
-        if salesMax <= float(thisSale):
             salesMax = float(thisSale);
-            oldKey = thisKey
-
-
+    else:
+        # If keys the same, compare sale from new line with current maximum sale
+        if salesMax <= float(thisSale): 
+            salesMax = float(thisSale);
+            oldKey = thisKey;
 
 # Capture last line to finish the loop 
-# if oldKey != None:
-#     print oldKey, "\t", salesMax
+if oldKey != None:
+    print oldKey, "\t", salesMax
 
 
