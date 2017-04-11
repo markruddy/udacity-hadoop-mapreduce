89d7f7dd1c60ceafdb7c79e164f9c697506cb2c8
markruddy <drmarkruddy@gmail.com>
2017-04-11 22:50:19 +0100

    Change output
    Split request into 3 parts, giving url as an output

--- logMapper.py
+++ logMapper.py
@@ -2,7 +2,7 @@
 
 # Mark Ruddy 
 # 2017-04-11 
-# mapper for Common Log Format file
+# Mapper for Common Log Format file
 
 import sys
 
@@ -10,6 +10,6 @@ for line in sys.stdin:
     data = line.strip().split(" ") # Split on non-breaking whitespace
     if len(data) == 10: # Defensive programming against non-standard log lines
     	# Combine 
-    	data_mapped = [data[0], data[1], data[2], data[3]+" "+data[4], data[5]+" "+data[6]+" "+data[7], data[8], data[9]]
-    	ipaddress, identity, username, time, request, status, size = data_mapped
-    print "{0}\t{6}".format(ipaddress, status)
+    	data_mapped = [data[0], data[1], data[2], data[3]+" "+data[4], data[5], data[6], data[7], data[8], data[9]]
+    	ipaddress, identity, username, time, requestStart, requestUrl, requestEnd, status, size = data_mapped
+    	print "{0}\t{1}".format(ipaddress, requestUrl)
